264d0db8b964e7ab74a1c71d55a41b46799cb40d
unknown <xarus82@mail.ru>
2023-04-17 15:35:22 +0300

    Java_OOP_Homework_5

---
+++ personal/Main.java
@@ -0,0 +1,17 @@
+package personal;
+
+import personal.model.FileOperation;
+import personal.model.FileOperationImpl;
+import personal.model.Repository;
+import personal.model.RepositoryFile;
+import personal.views.ViewUser;
+
+public class Main {
+    public static void main(String[] args) {
+        FileOperation fileOperation = new FileOperationImpl("users.txt");
+        Repository repository = new RepositoryFile(fileOperation);
+        userController controller = new UserController(repository);
+        ViewUser view = new ViewUser(controller);
+        view.run();
+    }
+}
